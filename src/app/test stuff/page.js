0efd2bbe0184a153d6cb58de98837c79c3259b0e
PyroAnnatar <pyroannatar@gmail.com>
2025-01-16 03:26:07 +0300

    forgetti testies spaghetti

--- src/app/test stuff/page.js
+++ src/app/test stuff/page.js
@@ -2,17 +2,17 @@
 import React from "react";
 import Sidebar from "../Sidebar";
 import Boards from "../components/Boards";
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
+// import { DndProvider } from "react-dnd";
+// import { HTML5Backend } from "react-dnd-html5-backend";
 
 const Home = () => {
   return (
-    <DndProvider backend={HTML5Backend}>
-      <div className="flex">
-        <Sidebar />
-        <Boards />
-      </div>
-    </DndProvider>
+    // <DndProvider backend={HTML5Backend}>
+    <div className="flex">
+      <Sidebar />
+      <Boards />
+    </div>
+    // </DndProvider>
   );
 };
 
